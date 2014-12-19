62dbd79612841bf676789aefae18e65006facb3f
clouddrd <clouddrd@gmail.com>
2014-12-19 09:15:26 +0900

    change

--- stage2/stage2.c
+++ stage2/stage2.c
@@ -2064,11 +2064,14 @@ static void rz_read_default_menu(void)
 {
 	int ret=0;
 	ret=grub_open("iboot.ini");
-	if(ret>0)printf("open:%d\n",ret);
+	if(ret!=0)printf("open:%d\n",ret);
 	else printf("error open:%d\n",ret);
 
 	ret = grub_read(rz_grub_menu_debug,10,0xedde0d90);
-	if(ret>0)printf("read:%d\n",ret);
+	rz_grub_menu_debug[10]=0;
+	printf("read:%d,%s\n",ret,rz_grub_menu_debug);
+
+	if(ret>0)printf("read:%d,%s\n",ret,rz_grub_menu_debug);
 	else printf("error read:%d\n",ret);
 
 	grub_close();
