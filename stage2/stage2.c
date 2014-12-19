546b99559b4f0fa05ed921c5f999b5947d2412d9
clouddrd <clouddrd@gmail.com>
2014-12-19 09:19:37 +0900

    test read default menu 1

--- stage2/stage2.c
+++ stage2/stage2.c
@@ -2064,15 +2064,12 @@ static void rz_read_default_menu(void)
 {
 	int ret=0;
 	ret=grub_open("iboot.ini");
-	if(ret!=0)printf("open:%d\n",ret);
-	else printf("error open:%d\n",ret);
+	if(!ret)printf("open error:%d\n",ret);
+	printf("open:%d\n",ret);
 
-	ret = grub_read(rz_grub_menu_debug,10,0xedde0d90);
+	grub_read(rz_grub_menu_debug,10,0xedde0d90);
 	rz_grub_menu_debug[10]=0;
 	printf("read:%d,%s\n",ret,rz_grub_menu_debug);
-
-	if(ret>0)printf("read:%d,%s\n",ret,rz_grub_menu_debug);
-	else printf("error read:%d\n",ret);
 
 	grub_close();
 	printf("close\n");
